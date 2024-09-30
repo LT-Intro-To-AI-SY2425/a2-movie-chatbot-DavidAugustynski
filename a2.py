586987953a6699ee6e1482f4548701a74c7e280e
David Augustynski <daugustynsk@cps.edu>
2024-09-30 11:09:51 -0500

    all test passed

--- a2.py
+++ a2.py
@@ -24,36 +24,44 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     while pind < len(pattern) or sind < len(source):
         # your job is to fill out the body of this loop
 
-        # you should delete the following line
-       
 
         # 1) if we reached the end of the pattern but not source
-        if pind == len(pattern) and sind != len(source):
+        if pind == len(pattern) and  sind < len(source):
             return None
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
-        elif pattern(pind) == "%":
-            if pind + 1 == len(pattern):
-                combined = " ".join(source[sind:])
-                result.append(combined)
+        elif pattern[pind] == '%':
+            if pind == len(pattern) - 1:
+                combined = ' '.join(source[sind:])
+                # result.append(combined)
+                result += [combined]
                 return result
             else:
-                return None
+                pind += 1
+                accum = ""
+                while source[sind] != pattern[pind]:
+                    accum += source[sind] + " "
+                    sind += 1
+                    # print(accum)
+                    
+                    if sind >= len(source):
+                        return None
+                result.append(accum.rstrip())
+            
         # 3) if we reached the end of the source but not the pattern
         elif sind == len(source) and pind < len(pattern):
             return None
         # 4) if the current thing in the pattern is an _
-        elif pattern[pind] == "_":
-           result.append(source[sind])
-           pind += 1
-           sind += 1
-           
+        elif pattern[pind] == '_':
+            result.append(source[sind])
+            pind += 1
+            sind += 1
 
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
-        elif pattern[pind] == pattern[sind]:
+        elif source[sind] == pattern[pind]:
             pind += 1
             sind += 1
         # 6) else : this will happen if none of the other conditions are met it
@@ -61,6 +69,7 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # source
         else:
             return None
+
     return result
 
 
